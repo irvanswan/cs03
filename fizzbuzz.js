1921a20e1b0be37f3440bf84514c8f79053c41f7
Affandes <affandes@uin-suska.ac.id>
2021-06-12 08:57:19 +0700

    Initial commit

---
+++ fizzbuzz.js
@@ -0,0 +1,7 @@
+class FizzBuzz {
+    printTo(max) {
+        console.log('Silahkan edit file fizzbuzz.js');
+    }
+}
+
+module.exports = FizzBuzz;
---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * File ini tidak perlu diubah.
+ * Silahkan ubah file fizzbuzz.js
+ */
+
+const FizzBuzz = require('./fizzbuzz');
+
+let max = 100;
+let fb = new FizzBuzz();
+fb.printTo(max);
