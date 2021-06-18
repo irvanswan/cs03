e028feb17bc987664d055446cc35a3416fce2896
Irvan Junaidi <49930719+irvanswan@users.noreply.github.com>
2021-06-18 11:09:42 +0000

    two task in one repositories

--- index.js
+++ index.js
@@ -4,6 +4,11 @@
  */
 
 const fizzBuzz = require('./fizzbuzz');
+const fizzBuzz2 = require('./fizzbuzz2');
 
 let max = 100;
+let data = [35,27,13,3,12,25,45];
+console.log('ini soal 1')
 fizzBuzz(max);
+console.log('ini soal 2')
+fizzBuzz2(data);
