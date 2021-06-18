52079104903eeab80a7f3f8f7c4fbae46fc4e9f0
Irvan Junaidi <49930719+irvanswan@users.noreply.github.com>
2021-06-18 10:47:52 +0000

    Complete the task Fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,19 @@
 const fizzBuzz = function(max) {
-    console.log('Silahkan ubah file fizzbuzz.js');
+  if(typeof(max)!=='number'){
+    console.log('tipe datanya bukan number !')
+  }else{
+    for(let i=1; i<=max; i++){
+      if(i % 3 ==0 && i % 5 ==0){
+        console.log(i,"FizzBuzz")
+      }else if(i%3==0){
+        console.log(i,"Fizz")
+      }else if(i%5==0){
+        console.log(i,"Buzz")
+      }else{
+        console.log(i)
+      }
+    }
+  }
 }
 
 module.exports = fizzBuzz;
